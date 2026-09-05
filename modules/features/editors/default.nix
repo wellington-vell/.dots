8f5c4244cceff820259655b782961874107c8d84
wellington-vell <wellingtonvell@gmail.com>
2026-09-05 20:23:30 -0300

    fix build

--- modules/features/editors/default.nix
+++ modules/features/editors/default.nix
@@ -7,7 +7,11 @@
     { pkgs, ... }:
     let
       # 26.05 ships Cursor 3.5.x with laggy SCM commit input; unstable is past the 3.8 fix.
-      unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+      # Import (not legacyPackages) so allowUnfree applies to this nixpkgs instance.
+      unstable = import inputs.nixpkgs-unstable {
+        inherit (pkgs.stdenv.hostPlatform) system;
+        config.allowUnfree = true;
+      };
     in
     {
       environment.sessionVariables.NIXOS_OZONE_WL = "1";
