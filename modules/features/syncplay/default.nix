f39a1707f4c07d44d917886013e7004878b62086
wellington-vell <wellingtonvell@gmail.com>
2026-09-07 21:02:14 -0300

    fix: path syncplay

--- modules/features/syncplay/default.nix
+++ modules/features/syncplay/default.nix
@@ -1,7 +1,17 @@
 {
   flake.modules.nixos.syncplay =
     { pkgs, ... }:
+    let
+      syncplay-patched = pkgs.syncplay.overrideAttrs (old: {
+        postPatch = (old.postPatch or "") + ''
+          substituteInPlace syncplay/constants.py \
+            --replace-fail 'VLC_PATHS = [' 'VLC_PATHS = ["vlc", "${pkgs.vlc}/bin/vlc", "/run/current-system/sw/bin/vlc",'
+        '';
+      });
+    in
     {
-      environment.systemPackages = [ pkgs.syncplay ];
+      environment.systemPackages = [
+        syncplay-patched
+      ];
     };
 }
